679a6b59d4099b6b417d58139b959735c78006fe
ponlawat <ponlawat@dollarsign.co>
2024-08-21 17:53:40 +0700

    fix(env): add env file

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly PUBLIC_APIURL: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
